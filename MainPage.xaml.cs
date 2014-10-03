9c8edfe4086d08d56b73e50d929f3fa853286bce
Anupamagulia <guliaanupama34@gmail.com>
2014-10-03 21:21:54 +0500

    Recipes app for windows phone
    It is a simple windows phone app.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace recipes
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void hyperlinkButton1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/idli2Page.xaml", UriKind.Relative));
+        }
+
+        private void hyperlinkButton2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/noodlesPage.xaml", UriKind.Relative));
+        }
+
+     
+       
+
+      
+      
+
+       
+      
+      
+
+       
+        
+    }
+}
